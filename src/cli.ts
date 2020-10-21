07c71b3d44c71922f37e33d42374f8afb05cf487
layouwen <layouwen@gmail.com>
2020-10-21 17:31:17 +0800

    update version

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,7 @@ import {translate} from './main';
 const program = new commander.Command();
 
 program
-  .version('0.0.1')
+  .version('0.0.2')
   .name('fy')
   .usage('<English>')
   .arguments('<English>')
